0dbc510946e0e125cf8e66436a51a27bfb12ab0b
Hou Zhiguo (uidq1723) <zhiguo.hou@desay-svautomotive.com>
2020-11-24 17:15:32 +0800

    update type and func practice

--- HelloWorld/main.go
+++ HelloWorld/main.go
@@ -1,10 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unsafe"
+	"math"
+) 
+
+type h =  int // means typedef in C++
+type hk int // hk means a new type, but hk has the feature of int
 
 func main() {
 	fmt.Println("Hello World")
-	var a int = 15
-	b := 16
-	fmt.Println( "a + b = ", (a+b) )
+	var a h = 10
+	var b int = 26
+	var c float64
+	var d string
+	var e hk
+	var f rune// rune is an alias for int32 and is equivalent to int32 in all ways
+	c, d = sqr_sum(a,b)
+	fmt.Println("sqr_sum(", a, ",", b, ")", "result:", c, "error:", d)
+	a = -20
+	b = 10
+	c, d = sqr_sum(a,b)
+	fmt.Println("sqr_sum(", a, ",", b, ")", "result:", c, "error:", d)
+
+	//package unsafe
+	fmt.Println( unsafe.Sizeof(f) )//unsafe.Sizeof return the number of bytes for var
+	fmt.Printf( "var a h type is: %T\n", a )
+	fmt.Printf( "var e hk type is: %T\n", e )
+}
+
+func sqr_sum(a int, b int) (ret float64, err string){// func: function keyword; (a int, b int): function argument; (int, int): return type, note golang allow multiple return value
+	var sum float64 = float64(a + b)
+	if sum > 0{
+		return math.Sqrt( sum ), "sucess"
+	}else{
+		return 0, "failed"
+	}
 }
