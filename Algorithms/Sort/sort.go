55b1a3dd3a1143015cb76d4d88af9c6ec1fb6231
hzgloveyd <15310405471@163.com>
2020-12-07 20:56:34 +0800

    add bubble sort algorithm

---
+++ Algorithms/Sort/sort.go
@@ -0,0 +1,33 @@
+package sort
+
+import(
+	"fmt"
+)
+
+func BubbleSort(input []int) (res []int, err error){
+	fmt.Println("BubbleSort input:", input)
+	count := 0
+	for i := 0; i < len(input); i++{
+		for j := len(input) - 1; j -i > 0; j--{
+			if input[j] < input[j-1]{
+				tmp := input[j]
+				input[j] = input[j-1]
+				input[j-1] = tmp				
+			}
+			count++
+		}
+	}
+	fmt.Println("calculate counter:", count, "input length:", len(input))
+	res = input
+	fmt.Println("BubbleSort output:", res)
+	err = nil
+	return
+}
+
+func QuickSort(input []int) (res []int, err error){
+	fmt.Println("QuickSort")
+
+	res = input
+	err = nil
+	return
+}
--- Algorithms/main.go
+++ Algorithms/main.go
@@ -2,8 +2,18 @@ package main
 
 import(
 	"fmt"
+	"math/rand"
+	"time"
+	"Algorithms/Sort"
 )
 
 func main(){
 	fmt.Println("Let's go to the world of algorithm")
-}
+	var a []int
+	rand.Seed(time.Now().Unix())
+	num := rand.Intn(45)
+	for i :=0; i < num; i++{
+		a = append(a, rand.Intn(1000))
+	}	
+	sort.BubbleSort(a)	
+}	
