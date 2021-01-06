e2a488e745f3b1c6f1defdd32b0158b6faed127c
hzgloveyd <15310405471@163.com>
2021-01-06 08:55:14 +0800

    update

--- Web/main.go
+++ Web/main.go
@@ -1,26 +1,119 @@
 package main
 
 import (
+	"crypto/tls"
+	"crypto/x509"
+	"io/ioutil"
 	"net/http"
 	"fmt"
+	"encoding/json"
+	"bytes"
+	"errors"
+	"sync"
 )
 
+var (
+	m = make(map[string] int)
+	lock sync.Mutex
+) 
+
+type JsonReponse  struct{
+    Response []bool `json:"Response"`
+}
+
+type JsonRequest struct{
+	Request []string `json:"Request"`
+}
+
 func BcjClient(input []string) (output []bool, err error){
-	output = append(output, true)
+	if len(input) <= 0 {
+		err = errors.New("the lenth of input []string <= 0!")
+		return
+	}
+	//output = append(output, true)
 	err = nil
+	var request JsonRequest
+	request.Request = input
+	msg, e := json.Marshal(request)
+	if e == nil {
+		pool := x509.NewCertPool()
+		caCertPath := "server.pem"
+		caCrt, er := ioutil.ReadFile(caCertPath)
+		if er != nil {
+			fmt.Println("ReadFile err:", er)
+			err = er
+			return
+		}
+		pool.AppendCertsFromPEM(caCrt)
+
+		tr := &http.Transport{
+			TLSClientConfig: &tls.Config{RootCAs: pool},
+		}
+
+		req := bytes.NewBuffer([]byte(msg))
+		reqh, _ := http.NewRequest("POST", "https://127.0.0.1:8081", req)
+		reqh.Header.Set("Content-type", "application/json")		 
+		client := &http.Client{Transport: tr}
+		resp, er := client.Do(reqh)
+		if er != nil {
+			fmt.Println("Get error:", er)
+			err = er
+			return
+		}
+		defer resp.Body.Close()
+		body, er := ioutil.ReadAll(resp.Body)
+		var ress JsonReponse
+		if err := json.Unmarshal(body, &ress); err == nil {
+			output = append(output, ress.Response...)
+		}
+	}	
 	return
 }
 
 func BcjServer(){
 	http.HandleFunc("/", handler)
-	http.ListenAndServeTLS(":8081", "server.crt", "server.key", nil)
+	http.ListenAndServeTLS(":8081", "server.pem", "server.key", nil)
 }
 
 func handler(w http.ResponseWriter, r *http.Request){
-	fmt.Fprintf(w,
-		"Hi, This is an example of https service in golang!")
+	body, _ := ioutil.ReadAll(r.Body)
+	var reqs JsonRequest
+	var ress JsonReponse
+	if err := json.Unmarshal(body, &reqs); err == nil{
+		fmt.Println("http.HandleFunc handler", reqs)
+		lock.Lock()
+		for _, r := range reqs.Request {			
+			_, ok := m[r]
+			if ok{
+				ress.Response = append(ress.Response, true)
+			}else{
+				m[r] = 0
+				ress.Response = append(ress.Response, false)
+			}
+		}
+		lock.Unlock()
+		ret, _ := json.Marshal(ress)
+		fmt.Fprintf(w, string(ret))
+	}else{		
+		s := "Can not recongnized the Params: "
+		s = s + string(body)  
+		fmt.Fprintf(w, s)
+	}	
 }
 
 func main(){
-	BcjServer()
+	ProductCerts()
+	go BcjServer()
+	var sa  = []string{"abc", "bdc", "dc", "ddc", "fbc", "ec"}
+	var output1, output2 []bool
+	output1, _ = BcjClient(sa)
+	fmt.Println("exe result: ", output1)
+	output2, _ = BcjClient(sa)
+	fmt.Println("exe result: ", output2)
+	var se []string
+	if _, err := BcjClient(se); err != nil {
+		fmt.Println(err)
+	}
+	for{
+	}
 }
--- Web/main_test.go
+++ Web/main_test.go
@@ -2,5 +2,38 @@ package main
 
 import(
 	"fmt"
+	"testing"
+	// "net/http/httptest"
+	// "net/http"
+	// "encoding/json"
 )
 
+func TestClient(t *testing.T){
+	var se []string
+	output, err := BcjClient(se)
+	if err != nil {
+		if err.Error() != "the lenth of input []string <= 0!" {		
+			t.Errorf("the error is not expacted and the err is :%s", err.Error())	
+		}else{
+			fmt.Printf("the length of output is actual[%d] expacted[0]", len(output))
+		}
+	}
+	go BcjServer()
+	var sa = []string{"abc", "bcd", "cde", "def", "abc"}
+	output, err = BcjClient(sa)
+	var expacted = []bool{false, false, false, false, true}
+	var rc = true 
+	if len(output) != 5{
+		rc = false
+	}
+	for i, val := range output{
+		if val != expacted[i]{
+			rc = false
+			break
+		}
+	}
+	if !rc {
+		t.Errorf("Error the expacted value is: false, false, false, true but the actual value is: ")
+		fmt.Println(output)
+	}
+}
